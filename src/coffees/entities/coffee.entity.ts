494214479cba48f20502c12cd8588c53f6fc1ac0
AhmedEid3 <ahmed.eid3@outlook.com>
2022-03-19 09:00:51 +0200

    Update Coffe Entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // by default sql table name is 'coffee'
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true }) // make flavors array as json and make column optional
   flavors: Array<string>;
 }
