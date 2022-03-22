0ce334d247242cb558886cd5e0f72cd87f9ad0e7
AhmedEid3 <ahmed.eid3@outlook.com>
2022-03-22 07:06:52 +0200

    Add CoffeeRating Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, CoffeeRatingModule],
   controllers: [AppController],
   providers: [AppService],
 })
